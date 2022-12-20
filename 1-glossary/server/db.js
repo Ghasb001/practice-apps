24d1c6a44064e07213c609cf5925b920fdc74da3
Guillermo Hasbun <hasbun.guillermo@gmail.com>
2022-12-19 22:29:51 -0500

    Add comgoose connection syntax change files

--- 1-glossary/server/db.js
+++ 1-glossary/server/db.js
@@ -1,6 +1,14 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
 
+mongoose.connect('mongodb+srv://Ghasb001:DEADspace!1991@cluster0.4ygl3zq.mongodb.net/?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true})
+.then(() => {
+  console.log('db connected')
+})
+.catch((err) => {
+  console.log('Error', err)
+})
+
 // 1. Use mongoose to establish a connection to MongoDB
 // 2. Set up any schema and models needed by the app
 // 3. Export the models
