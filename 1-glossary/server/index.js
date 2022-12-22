e219e029ef4c09f168377b3200f5ee376ec56942
Guillermo Hasbun <hasbun.guillermo@gmail.com>
2022-12-21 21:52:08 -0500

    Install express

--- 1-glossary/server/index.js
+++ 1-glossary/server/index.js
@@ -1,1 +1,19 @@
 require("dotenv").config();
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.static('client/dist'));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.sendStatus(200);
+  res.end();
+})
+
+
+let port = 3000;
+
+app.listen(port, function() {
+  console.log(`listening on port ${port}`);
+});
