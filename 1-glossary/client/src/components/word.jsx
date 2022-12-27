ca6aedfbd0f6d6b0eea3082fa8a95191f1e0de13
Guillermo Hasbun <hasbun.guillermo@gmail.com>
2022-12-27 09:18:30 -0500

    Add buttons

--- 1-glossary/client/src/components/word.jsx
+++ 1-glossary/client/src/components/word.jsx
@@ -3,6 +3,12 @@ import React, { Component} from "react";
 const Word = (props) => (
   <div >
     <h5> WORD </h5>
+    <button type="button" onClick={() => {
+          alert('Delete')
+        }}>DELETE</button>
+        <button type="button" onClick={() => {
+          alert('edit')
+        }}>EDIT</button>
   </div>
 )
 
