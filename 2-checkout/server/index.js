8ad44262a55d0c736638b5111de9fa9465fbaedf
Guillermo Hasbun <hasbun.guillermo@gmail.com>
2022-12-31 14:35:02 -0500

    Add some dependencies

--- 2-checkout/server/index.js
+++ 2-checkout/server/index.js
@@ -19,12 +19,12 @@ app.use(logger);
 // Serves up all static and generated assets in ../client/dist.
 app.use(express.static(path.join(__dirname, "../client/dist")));
 
-/**** 
- * 
- * 
+/****
+ *
+ *
  * Other routes here....
  *
- * 
+ *
  */
 
 app.listen(process.env.PORT);
