68c3ce566e0098a856ddb044d855bd5297364cab
Guillermo Hasbun <hasbun.guillermo@gmail.com>
2023-01-03 22:35:36 -0500

    Make button flow work

--- 2-checkout/client/src/components/app.jsx
+++ 2-checkout/client/src/components/app.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
-import F1 from './f1.jsx';
+//import F1 from './f1.jsx';
 
 // boolean checks to determine the form number
 var checkoutClick = false;
 var f1Click = false;
+
+var data = {};
 
 var App = () => {
   // const [page, setPage] = useState(null);
@@ -14,33 +16,88 @@ var App = () => {
     console.log(e);
   }
 
-  const [open, setIsOpen] = React.useState(null);
+  const [form, setForm] = useState(null);
+
+  //const [visible, setVisible] = useState(true);
+  // const removeElement = () => {
+  //   setVisible((prev) => !prev);
+  //   deleteData(props);
+  // };
 
 
-let rerender = () => {
-  var page = (<div>
-    <h2>Now?</h2>
-  </div>)
-  setIsOpen(page);
+let F1Render = () => {
+  var F1 = (
+  <form>
+    <label>
+      Name:
+      <input type="text" name="name" />
+      Email:
+      <input type="text" email="email" />
+      Password:
+      <input type="text" password="password" />
+    </label>
+    <button onClick={(e) => {
+      e.preventDefault();
+      F2Render();
+    }}> Next </button>
+    </form>
+    )
+  setForm(F1);
 }
 
-// useEffect(() => (
-//   rerender()
-// ), null)
+let F2Render = () => {
+  var F2 = (
+  <form >
+    <label>
+      Line 1:
+      <input type="text" l1="l1" />
+      Line 2:
+      <input type="text" l2="l2" />
+      City:
+      <input type="text" city="city" />
+      State:
+      <input type="text" state="state" />
+      Zip Code:
+      <input type="text" zip="zip" />
+    </label>
+    <button onClick={(e) => {
+      e.preventDefault();
+      F3Render();
+    }}> Next </button>
+    </form>
+    )
+  setForm(F2);
+}
 
+let F3Render = () => {
+  var F3 = (<form>
 
+    <label>
+      Credit Card Number:
+      <input type="text" cc="cc" />
+      Exp. Date:
+      <input type="text" exp="exp" />
+      CVV (3 numbers on the back of your card):
+      <input type="text" cvv="cvv" />
+      Billing Zip Code:
+      <input type="text" billzip="billzip" />
+    </label>
+    <button onClick={(e) => {
+      e.preventDefault();
+      //F2Render();
+      alert('Purchase Complete!')
+    }}> Purchase </button>
+    </form>
+    )
+  setForm(F3);
+}
 
   // item1 LOGIC item2 ? firstCondition : secondCondition
 
   return (<div>
 
-    <div>
-        <h2>F1 False</h2>
-        <button type="F1 Submit" onClick={checkSub}> Checkout</button>
-      </div>
-
-      <button onClick={rerender}> click me! </button>
-      <h3>{open}</h3>
+      <button onClick={F1Render}> Checkout </button>
+      <h3>{form}</h3>
 
   </div>)
 }
